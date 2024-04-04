98d4f975521e4e4175ac89b7106fe2a8b0f6c7d4
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-04-03 20:59:44 -0600

    Refactor SidebarItem component styles

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -19,12 +19,23 @@ export default function SidebarItem({
   description,
 }: SidebarItem): JSX.Element {
   const currentPath = usePathname();
+  const isActive = currentPath === path;
+
+  // Define a base style with transition
+  let baseStyle = 'flex items-center w-full p-3 rounded-lg text-start leading-tight transition duration-300 ';
+
+  // Define styles for hover, focus, and active states
+  let hoverFocusActiveStyle = 'hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900';
+
+  // Define active style
+  let activeStyle = 'bg-blue-100 text-blue-900'; // Example active style
+
   return (
     <Link href={path}>
       <span
         role='button'
         tabIndex={0}
-        className='flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none'
+        className={`${baseStyle} ${isActive ? activeStyle : ''} ${hoverFocusActiveStyle}`}
         onClick={onClick}
       >
         <div className='grid place-items-center mr-4'>{icon}</div>
