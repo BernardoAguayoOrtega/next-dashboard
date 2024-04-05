9db622dead9e424b62f157467b094d707877c82a
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-04-04 20:31:46 -0600

    Update CounterPage component to display cart products and provide buttons to increment and decrement quantity

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -2,7 +2,17 @@
 export default function CounterPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <h1>Counter Page</h1>
+      <span>Cart Products</span>
+      <span className="text-9xl">10</span>
+
+      <div className="flex space-x-5">
+        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-400 transition-all w-[100px] mr2">
+          +1
+        </button>
+        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-400 transition-all w-[100px] mr2">
+          -1
+        </button>
+      </div>
     </div>
   );
 }
